acbacc345a9c953fe54787c756a904f8b381bdaa
villamorchiavarone6-svg <villamorchiavarone6@gmail.com>
2026-02-07 09:47:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,13 @@
 import os
-import requests
-import time
-import threading
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# /start
 async def start(update, context):
     await update.message.reply_text("🤖 Bot attivo!")
 
-# /showid → SERVE SOLO PER PRENDERE IL CHAT ID
-async def showid(update, context):
-    chat = update.effective_chat
-    await update.message.reply_text(f"👀 Chat ID: {chat.id}")
-
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("showid", showid))
 
 app.run_polling()
